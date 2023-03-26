3b7e7c66d8232eb04a563abb6c2f5e3f66f7f638
q10sn1k <kondratenkona23@gmail.com>
2023-03-26 22:10:02 +0300

    add PostList

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Register from "./components/Register";
 import Login from "./components/Login";
+import PostList from "./components/PostList";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Register />
       <Login />
+      <PostList />
     </div>
   );
 }
