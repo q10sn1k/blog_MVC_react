d114d83d20fed7fa858e6cc88aeb3e5cb45427b0
q10sn1k <kondratenkona23@gmail.com>
2023-03-26 21:11:07 +0300

    add register & test

---
+++ client/src/components/tests/Register.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render, fireEvent, screen } from '@testing-library/react';
+import Register from '../Register';
+
+describe('Register', () => {
+  test('содержит форму регистрации', () => {
+    render(
+      <BrowserRouter basename="/">
+        <Register />
+      </BrowserRouter>
+    );
+    const usernameInput = screen.getByLabelText('Имя пользователя');
+    expect(usernameInput).toBeInTheDocument();
+    const emailInput = screen.getByLabelText('Email');
+    expect(emailInput).toBeInTheDocument();
+    const passwordInput = screen.getByLabelText('Пароль');
+    expect(passwordInput).toBeInTheDocument();
+    const passwordConfirmationInput = screen.getByLabelText('Подтверждение пароля');
+    expect(passwordConfirmationInput).toBeInTheDocument();
+    const submitButton = screen.getByRole('button', { name: 'Зарегистрироваться' });
+    expect(submitButton).toBeInTheDocument();
+  });
+});
