82b6dc24b8f1aeaa7a1e3e9548aa2f5aa72723a2
q10sn1k <kondratenkona23@gmail.com>
2023-03-26 22:24:38 +0300

    add PostForm

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Register from "./components/Register";
 import Login from "./components/Login";
 import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Register />
       <Login />
       <PostList />
+      <PostForm />
     </div>
   );
 }
--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import './PostList.css';
 
 const PostList = () => {
   const [posts, setPosts] = useState([]);
